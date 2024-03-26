d64befacdfa70a6a87e695450ff50615cc1be7e7
Matthew Hoover <matthewhoover13@gmail.com>
2024-03-26 17:48:15 -0500

    added a test music disc (DS Twilight House from MKWii) to help test out future music player block

--- src/main/java/net/hoover/musicplayer/MusicPlayer.java
+++ src/main/java/net/hoover/musicplayer/MusicPlayer.java
@@ -2,6 +2,8 @@ package net.hoover.musicplayer;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.hoover.musicplayer.item.ModItems;
+import net.hoover.musicplayer.sound.ModSounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class MusicPlayer implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+		ModSounds.registerSounds();
 	}
 }
