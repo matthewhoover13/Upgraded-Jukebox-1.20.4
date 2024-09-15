c27be03acd5c8b976a4b7685e6b83714083f40d8
Matthew Hoover <matthewhoover13@gmail.com>
2024-09-15 15:59:54 -0500

    made the music player block 2 blocks tall and directional

--- src/main/java/net/hoover/musicplayer/block/custom/MusicPlayerBlock.java
+++ src/main/java/net/hoover/musicplayer/block/custom/MusicPlayerBlock.java
@@ -7,24 +7,81 @@ import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.block.enums.DoubleBlockHalf;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemStack;
 import net.minecraft.screen.NamedScreenHandlerFactory;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.Hand;
-import net.minecraft.util.ItemScatterer;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.*;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
+import net.minecraft.world.WorldView;
 import org.jetbrains.annotations.Nullable;
 
 public class MusicPlayerBlock extends BlockWithEntity implements BlockEntityProvider {
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+    public static final EnumProperty<DoubleBlockHalf> HALF = Properties.DOUBLE_BLOCK_HALF;
+
     public MusicPlayerBlock(Settings settings) {
         super(settings);
+        setDefaultState(getDefaultState().with(Properties.HORIZONTAL_FACING, Direction.NORTH).with(Properties.DOUBLE_BLOCK_HALF, DoubleBlockHalf.LOWER));
     }
 
     @Override
     protected MapCodec<? extends BlockWithEntity> getCodec() {
         return null;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(Properties.HORIZONTAL_FACING);
+        builder.add(Properties.DOUBLE_BLOCK_HALF);
+    }
+
+    @Override
+    public boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+        BlockPos blockPos = pos.down();
+        BlockState blockState = world.getBlockState(blockPos);
+        if (state.get(HALF) == DoubleBlockHalf.LOWER) {
+            return blockState.isSideSolidFullSquare(world, blockPos, Direction.UP);
+        }
+        return blockState.isOf(this);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        BlockPos blockPos = ctx.getBlockPos();
+        World world = ctx.getWorld();
+        if (blockPos.getY() < world.getTopY() - 1 && world.getBlockState(blockPos.up()).canReplace(ctx)) {
+            return this.getDefaultState().with(FACING, ctx.getHorizontalPlayerFacing().getOpposite()).with(HALF, DoubleBlockHalf.LOWER);
+        }
+        return null;//super.getPlacementState(ctx).with(Properties.HORIZONTAL_FACING, ctx.getHorizontalPlayerFacing().getOpposite());
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        world.setBlockState(pos.up(), state.with(HALF, DoubleBlockHalf.UPPER), Block.NOTIFY_ALL);
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
     }
 
     @Override
@@ -53,12 +110,44 @@ public class MusicPlayerBlock extends BlockWithEntity implements BlockEntityProv
     @Override
     public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, Hand hand, BlockHitResult hit) {
         if (!world.isClient) {
-            NamedScreenHandlerFactory screenHandlerFactory = ((MusicPlayerBlockEntity) world.getBlockEntity(pos));
+            NamedScreenHandlerFactory screenHandlerFactory = ((MusicPlayerBlockEntity) world.getBlockEntity(state.get(HALF) == DoubleBlockHalf.LOWER ? pos : pos.down()));
             if (screenHandlerFactory != null) {
                 player.openHandledScreen(screenHandlerFactory);
             }
         }
         return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        if (!world.isClient) {
+            if (state.get(HALF) == DoubleBlockHalf.UPPER) {
+                if (world.getBlockState(pos.down()).getBlock() instanceof MusicPlayerBlock && world.getBlockState(pos.down()).get(HALF) == DoubleBlockHalf.LOWER) {
+                    world.setBlockState(pos.down(), Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL | Block.SKIP_DROPS);
+                    world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, pos.down(), Block.getRawIdFromState(world.getBlockState(pos.down())));
+                }
+            } else {
+                world.syncWorldEvent(WorldEvents.JUKEBOX_STOPS_PLAYING, pos, 0);
+                if (world.getBlockState(pos.up()).getBlock() instanceof MusicPlayerBlock && world.getBlockState(pos.up()).get(HALF) == DoubleBlockHalf.UPPER) {
+                    world.setBlockState(pos.up(), Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL | Block.SKIP_DROPS);
+                    world.syncWorldEvent(player, WorldEvents.BLOCK_BROKEN, pos.up(), Block.getRawIdFromState(world.getBlockState(pos.up())));
+                }
+            }
+        }
+        return super.onBreak(world, pos, state, player);
+    }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        return super.getOutlineShape(state, world, pos, context);
+        /*Direction dir = state.get(FACING);
+        return switch (dir) {
+            case NORTH -> VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.5f);
+            case SOUTH -> VoxelShapes.cuboid(0.0f, 0.0f, 0.5f, 1.0f, 1.0f, 1.0f);
+            case EAST -> VoxelShapes.cuboid(0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+            case WEST -> VoxelShapes.cuboid(0.0f, 0.0f, 0.0f, 0.5f, 1.0f, 1.0f);
+            default -> VoxelShapes.fullCube();
+        };*/
     }
 
     @Nullable
